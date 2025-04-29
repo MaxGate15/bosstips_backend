8a313a4e6c29b4a9f2ded29939ba87181d355ba6
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-29 22:41:37 +0000

    set up underway

--- wonitBackend/settings.py
+++ wonitBackend/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import dj_database_url
 
 from pathlib import Path
 
@@ -87,12 +88,17 @@ WSGI_APPLICATION = 'wonitBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=1800
+    )
 }
+
+
 
 
 # Password validation
