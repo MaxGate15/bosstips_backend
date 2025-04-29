9745cbd1e6391b6f4a99012a75844b830f257680
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-29 02:33:31 +0000

    set up underway

--- wonitApi/urls.py
+++ wonitApi/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns = [
-    path('games/',get_todays_games)
+    path('games/',get_todays_games),
+    path('codes/',get_booking_code)
 ]
