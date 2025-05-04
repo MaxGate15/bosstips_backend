abe274b8863fcce5c5b8b1cb6c30f09aa4bdaca3
Kobby24 <geffah005@st.ug.edu.gh>
2025-05-04 22:54:40 +0000

    set up underway

--- wonitBackend/settings.py
+++ wonitBackend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
     'wonitApi',
     'corsheaders'  # ✅ Add this line
 
