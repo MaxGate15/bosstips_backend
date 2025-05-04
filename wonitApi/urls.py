bd486d9b35d82ecfff6f9ca068a8ccf62aebda94
Kobby24 <geffah005@st.ug.edu.gh>
2025-05-04 18:32:10 +0000

    set up underway

--- wonitApi/urls.py
+++ wonitApi/urls.py
@@ -1,10 +1,21 @@
+from django.contrib import admin
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from .views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+router = DefaultRouter()
 
 urlpatterns = [
+    path("csrf/",get_csrf),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
     path('today-games/',TodaysGamesView.as_view(),name='todays-game'),
     path('tomorrow-games/',TomorrowGamesView.as_view(),name='tomorrows-game'),
-    path('yesterdays-games/',YesterdayGamesView.as_view(),name='yesterdays-game'),
+    path('yesterday-games/',YesterdayGamesView.as_view(),name='yesterdays-game'),
     path('other-games',YesterdayGamesView.as_view()),
     path('codes/',get_booking_code)
 ]
