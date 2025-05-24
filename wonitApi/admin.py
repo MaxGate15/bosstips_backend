555fe10e5f1f25fe2d8a0e4f07cda050763c7f63
Kobby24 <geffah005@st.ug.edu.gh>
2025-05-24 23:50:44 +0000

    set up underway

--- wonitApi/admin.py
+++ wonitApi/admin.py
@@ -4,8 +4,31 @@ from .models import *
 
 admin.site.register(Games)
 admin.site.register(BookingCode)
+admin.site.register(Purchase)
 
 
 @admin.register(Slips)
 class SlipsAdmin(admin.ModelAdmin):
     filter_horizontal = ('games',)  # OR use filter_vertical
+
+
+from django.contrib import admin
+from .models import Notifications
+
+class NotificationsAdmin(admin.ModelAdmin):
+    def has_module_permission(self, request):
+        return request.user.is_superuser
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+admin.site.register(Notifications, NotificationsAdmin)
